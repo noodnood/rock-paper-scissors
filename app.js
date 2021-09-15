c879324e1fee74d86bfa992687040c98c91fe227
Min Shuen <leeminshuen.lms@gmail.com>
2021-09-15 15:09:39 +0800

    Added win/lose tracking

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 console.log("Hello World!");
 
+  // Initialize initial scores
+  var playerScore = 0;
+  var computerScore = 0;
+
 // function for computer's move
 function computerPlay() {
   const movesArray = ["Rock", "Paper", "Scissors"];
   const randomMove = Math.floor(Math.random() * movesArray.length);
-//   console.log(randomMove, movesArray[randomMove]);
+  //   console.log(randomMove, movesArray[randomMove]);
 
   return movesArray[randomMove];
 }
@@ -23,33 +27,46 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
     // Win Cases
-    console.log(`You Win! ${capitalize(playerSelection)} beats ${computerSelection}!`);
+    playerScore++;
+    console.log(
+      `You Win! ${capitalize(playerSelection)} beats ${computerSelection}!`
+    );
   } else {
     // Lose Cases
-    console.log(`You Lose! ${capitalize(computerSelection)} beats ${playerSelection}!`);
+    computerScore++;
+    console.log(
+      `You Lose! ${capitalize(computerSelection)} beats ${playerSelection}!`
+    );
   }
 }
 
-
-function game () {
-    // Initialize initial scores
-    var playerScore = 0;
-    var computerScore = 0;
-    
-    // Game is played for 5 rounds
-    for (let i = 0; i < 5; i++) {
-        var playerSelection = prompt("Please choose your move: Rock / Paper / Scissors", "Rock");
-        playRound(playerSelection, computerPlay());
-
-    }
+function game() {
+  // Game format: Best of 5
+  for (let i = 0; i < 5; i++) {
+    var playerSelection = prompt(
+      "Please choose your move: Rock / Paper / Scissors",
+      "Rock"
+    );
+    playRound(playerSelection, computerPlay());
+  }
+  console.log(declareResults(playerScore, computerScore));
 }
-
-
-
 
 // Helper Functions
 
 // Function to capitalize a string
 function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1);
+}
+
+function declareResults (playerScore, computerScore) {
+  let finalScoreMessage = `Player ${playerScore} | Computer ${computerScore}`;
+
+  if (playerScore < computerScore) {
+    return "You lose! " + finalScoreMessage;
+  } else if (computerScore < playerScore) {
+    return "You win! " + finalScoreMessage;
+  } else {
+    return "It's a tie! " + finalScoreMessage;
+  }
 }
